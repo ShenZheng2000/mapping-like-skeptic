a81c6d39749bc2410e1858cf4afaa6203fcf4c4a
Shen Zheng <shenzhen@trinity-0-18.eth>
2026-06-25 21:19:40 -0400

    fix av2 mobilenet error

--- run.sh
+++ run.sh
@@ -1,18 +1,20 @@
 ulimit -n 65536
 
-# TODO: later, if having time, retrain this version (paper reported is 4 GPUs, i think! )
+# python tools/data_converter/argoverse_converter.py --data-root datasets/argoverse2_geosplit
+
 # bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage1.py 4
 # bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage2.py 4
 # bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage3.py 4
 
-bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage1_mobilenetv3.py 4
-bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage2_mobilenetv3.py 4
-bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage3_mobilenetv3.py 4
+# bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage1_mobilenetv3.py 4
+# bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage2_mobilenetv3.py 4
+# bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage3_mobilenetv3.py 4
 
 # bash tools/dist_test.sh  \
 #   plugin/configs/skeptic/av2_newsplit/stage3_mobilenetv3.py    \
 #   work_dirs/stage3_mobilenetv3/latest.pth  \
 #   4 --eval
+
 
 
 # python tools/data_converter/nuscenes_converter.py --data-root ./datasets/nuscenes --geosplit
@@ -21,17 +23,10 @@ bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage3_mobilenetv
 # bash ./tools/dist_train.sh plugin/configs/skeptic/nuscenes_newsplit/mls_nusc_new_3_joint_finetune.py 8
 
 # bash ./tools/dist_train.sh plugin/configs/skeptic/nuscenes_newsplit/mls_nusc_new_1_bev_pretrain_mobilenetv3.py 8
-# TODO: retrain these with the correct config!
 # bash ./tools/dist_train.sh plugin/configs/skeptic/nuscenes_newsplit/mls_nusc_new_2_warmup_mobilenetv3.py 8
 # bash ./tools/dist_train.sh plugin/configs/skeptic/nuscenes_newsplit/mls_nusc_new_3_joint_finetune_mobilenetv3.py 8
-
 
 # bash tools/dist_test.sh  \
 #   plugin/configs/skeptic/nuscenes_newsplit/mls_nusc_new_3_joint_finetune_mobilenetv3.py    \
 #   work_dirs/mls_nusc_new_3_joint_finetune_mobilenetv3/latest.pth  \
-#   8  --eval
-
-# bash tools/dist_test.sh  \
-#   plugin/configs/skeptic/nuscenes_newsplit/mls_nusc_new_2_warmup.py    \
-#   work_dirs/mls_nusc_new_2_warmup/latest.pth  \
 #   8  --eval
