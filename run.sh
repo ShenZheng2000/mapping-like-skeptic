178e70f388926ac3b686011573d1f23ebbcb07b6
Shen Zheng <shenzhen@trinity-3-8.eth>
2026-07-10 19:17:02 -0400

    Add AV2 geo-split 100x50 stage configs and run script

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 ulimit -n 65536
 
+############################# Argoverse2 Experiments #############################
 # python tools/data_converter/argoverse_converter.py --data-root datasets/argoverse2_geosplit
+
+# Preparing 100x50 symlink so to generate different tracking files
+# cd /scratch/shenzhen/Datasets/argoverse2_geosplit
+# ln -s av2_map_infos_train.pkl av2_map_infos_train_100x50.pkl
+# ln -s av2_map_infos_val.pkl av2_map_infos_val_100x50.pkl
+# then, proceed with prepare_gt_tracks.py ...
 
 # train
 # bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage1.py 4
@@ -10,7 +17,7 @@ ulimit -n 65536
 # train (100m x 50m)
 # Maybe: also need to prepare different tracking files for 100m x 50m, if error occurs during training. 
 # bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage1_100x50.py 4
-bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage2_100x50.py 4
+# bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage2_100x50.py 4
 bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage3_100x50.py 4
 
 # train (mobilenetv3 backbone)
@@ -27,6 +34,8 @@ bash ./tools/dist_train.sh plugin/configs/skeptic/av2_newsplit/stage3_100x50.py 
 #   4 --eval
 
 
+
+############################# nuScenes Experiments #############################
 # NOTE: nuScenes-map-expansion-v1.3.zip and v1.0-trainval-meta.tgz are required, before all the following steps! 
 # python tools/data_converter/nuscenes_converter.py --data-root ./datasets/nuscenes --geosplit
 
